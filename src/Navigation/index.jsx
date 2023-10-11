04fd0690ef8bfde54e154719f35b7de1f942c506
juanjzb <zb.juanjose@gmail.com>
2023-10-10 20:43:58 -0600

    Cambiando rutas absolutas a rutas relativas

--- src/Navigation/index.jsx
+++ src/Navigation/index.jsx
@@ -1,34 +1,30 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LogIn from 'C:/Users/GWTC/Desktop/SaludMovil/src/screens/SignIn/LogIn';
-import SignUp from 'C:/Users/GWTC/Desktop/SaludMovil/src/screens/SignUp/SignUp';
-import ConfirmEmail from '../screens/ConfirmEmail';
-import ForgotPassword from '../screens/ForgotPassword';
-import NewPassword from '../screens/NewPassword';
-import HomeScreen from '../screens/HomeScreen/HomeScreen';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import LogIn from '../screens/SignIn/LogIn'
+import SignUp from '../screens/SignUp/SignUp'
+import ConfirmEmail from '../screens/ConfirmEmail'
+import ForgotPassword from '../screens/ForgotPassword'
+import NewPassword from '../screens/NewPassword'
+import HomeScreen from '../screens/HomeScreen/HomeScreen'
 
-const Stack = createNativeStackNavigator();
-
+const Stack = createNativeStackNavigator()
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name= "SignIn" component= {LogIn} />
-        <Stack.Screen name= "SignUp" component= {SignUp} />
-        <Stack.Screen name= "ConfirmEmail" component= {ConfirmEmail} />
-        <Stack.Screen name= "ForgotPassword" component= {ForgotPassword} />
-        <Stack.Screen name= "NewPassword" component= {NewPassword} />
-        <Stack.Screen name= "HomeScreen" component= {HomeScreen} />
-      </Stack.Navigator> 
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='SignIn' component={LogIn} />
+        <Stack.Screen name='SignUp' component={SignUp} />
+        <Stack.Screen name='ConfirmEmail' component={ConfirmEmail} />
+        <Stack.Screen name='ForgotPassword' component={ForgotPassword} />
+        <Stack.Screen name='NewPassword' component={NewPassword} />
+        <Stack.Screen name='HomeScreen' component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
-  );
-};
-
-
-
+  )
+}
 
 export default Navigation
 
